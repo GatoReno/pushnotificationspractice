4f7d6592e435fbd72f219dcd3917e3321b007815
Gatoreno <erp.reno@hotmail.com>
2020-11-05 12:00:13 -0500

    adding ios implementation

---
+++ AptivistPush/App.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Plugin.FirebasePushNotification;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AptivistPush
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+            CrossFirebasePushNotification.Current.Subscribe("general");
+            CrossFirebasePushNotification.Current.OnTokenRefresh += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine($"TOKEN REC: {p.Token}");
+            };
+            System.Diagnostics.Debug.WriteLine($"TOKEN: {CrossFirebasePushNotification.Current.Token}");
+
+            CrossFirebasePushNotification.Current.OnNotificationReceived += (s, p) =>
+            {
+                try
+                {
+                    System.Diagnostics.Debug.WriteLine("Received");
+                    if (p.Data.ContainsKey("body"))
+                    {
+                        Device.BeginInvokeOnMainThread(() =>
+                        {
+                            //mPage.Message = $"{p.Data["body"]}";
+                        });
+
+                    }
+                }
+                catch (Exception ex)
+                {
+
+                }
+
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationOpened += (s, p) =>
+            {
+                //System.Diagnostics.Debug.WriteLine(p.Identifier);
+
+                System.Diagnostics.Debug.WriteLine("Opened");
+                foreach (var data in p.Data)
+                {
+                    System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                }
+
+                if (!string.IsNullOrEmpty(p.Identifier))
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        // mPage.Message = p.Identifier;
+                    });
+                }
+                else if (p.Data.ContainsKey("color"))
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        //mPage.Navigation.PushAsync(new ContentPage()
+                        //{
+                        //    BackgroundColor = Color.FromHex($"{p.Data["color"]}")
+
+                        //});
+                    });
+
+                }
+                else if (p.Data.ContainsKey("aps.alert.title"))
+                {
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        // mPage.Message = $"{p.Data["aps.alert.title"]}";
+                    });
+
+                }
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationAction += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Action");
+
+                if (!string.IsNullOrEmpty(p.Identifier))
+                {
+                    System.Diagnostics.Debug.WriteLine($"ActionId: {p.Identifier}");
+                    foreach (var data in p.Data)
+                    {
+                        System.Diagnostics.Debug.WriteLine($"{data.Key} : {data.Value}");
+                    }
+
+                }
+
+            };
+
+            CrossFirebasePushNotification.Current.OnNotificationDeleted += (s, p) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Dismissed");
+            };
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
